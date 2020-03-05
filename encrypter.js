d6cda9b3ab8c67eb1bf0f3623d82a96ffce0b31c
pabble <49900766+pabble@users.noreply.github.com>
2020-03-05 10:45:55 +0100

    initial commit

---
+++ encrypter.js
@@ -0,0 +1,39 @@
+var unicode = [];
+for (i = 0; i < 1114112; i++) { unicode.push(String.fromCharCode(i)); };
+
+var seed = {
+    dec: 777,
+    bin: 0100100
+}
+
+function encrypt (string, seed) {
+    var encrypted = string;
+    return encrypted;
+}
+
+console.log(encrypt("penis", seed));
+
+function offsetarray (array, index, sum, sign){
+    var result = 0;
+    if (sign == 1){
+        var result = index + sum;
+    }else{
+        var result = index - sum;
+    }
+
+    if (sign == 1) {
+        if (result > array.length){
+            while (result > array.length){
+                result -= array.length;
+            }
+        }
+    }
+
+    if (sign == 0) {
+        if (result < 0){
+            while (result < 0){
+                result += array.length;
+            }
+        }
+    }
+}
